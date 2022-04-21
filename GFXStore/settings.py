a1f92661410fc0ae8ab0d57597a23615ea9da8ca
PTS <76948129+PTSProgrammer@users.noreply.github.com>
2022-04-21 12:17:08 +0545

    set

--- GFXStore/settings.py
+++ GFXStore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-h3#ckuy-_qje%x(jacfsngc^^xo#$!b(p74m*g$w!jg#swmot=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
