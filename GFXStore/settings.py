45614e8ce4deb465ca8be3f5b44eecbefe4253f7
PTS <76948129+PTSProgrammer@users.noreply.github.com>
2022-04-22 15:01:06 +0545

    static

--- GFXStore/settings.py
+++ GFXStore/settings.py
@@ -142,7 +142,7 @@ STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 STATIC_URL = STATIC_HOST + "/static/"
 MEDIA_ROOT = "media"
 MEDIA_URL = "/media/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
